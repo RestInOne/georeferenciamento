f5f0022bec887e2251b7b521abb9b4e6f5fdbaa2
Guilherme Vitchenzo Penas <gvitchenzo@gmail.com>
2023-03-03 11:46:34 -0300

    feat: start sidebar development

--- src/components/sidebar/styled.ts
+++ src/components/sidebar/styled.ts
@@ -1,42 +1,59 @@
 import styled from "styled-components";
+import * as Select from '@radix-ui/react-select'
 
-export const Nav = styled.nav`
-    position: absolute;
-    top: 5rem;
-    left: 2rem;
-    height: 85vh;
-    width: max-content;
-    display: flex;
-    align-items: center;
-    gap: 3rem;
-    padding: 3rem;
-    flex-direction: column;
-    border-radius: .5rem;
-    border: .2rem solid black;
-    z-index: 3;
-    background-color: white;
-`
+export const Root = styled(Select.Root)``
 
-export const Label = styled.label`
-    font-weight: 700;
-    font-size: 24px;
-`
+export const Trigger = styled(Select.SelectTrigger)``
 
-export const ConditionFilter = styled.select`
-    border-top: 2px solid black;
-    border-bottom: 2px solid black;
-    border-top-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-    width: 25rem;
-    border-left: none;
-    border-right: none;
-    font-size: 18px;
-    padding: 2rem;
-`
+export const SelectIcon = styled(Select.SelectIcon)``
 
-export const CondititionSelector = styled.option`
-    border-top: 1px solid black;
-    border-bottom: 1px solid black;
-    width: 30rem;
-    font-size: 18px;
-`
+export const Value = styled(Select.Value)``
+
+export const Portal = styled(Select.Portal)``
+
+export const Content = styled(Select.Content)``
+
+
+
+
+
+// export const Nav = styled.nav`
+//     position: absolute;
+//     top: 5rem;
+//     left: 2rem;
+//     height: 85vh;
+//     width: max-content;
+//     display: flex;
+//     align-items: center;
+//     gap: 3rem;
+//     padding: 3rem;
+//     flex-direction: column;
+//     border-radius: .5rem;
+//     z-index: 3;
+//     background-color: #FFFDF7;
+// `
+
+// export const Label = styled.label`
+//     font-weight: 700;
+//     font-size: 24px;
+//     color: #73726F;
+// `
+
+// export const ConditionFilter = styled.select`
+//     border-top: 2px solid black;
+//     border-bottom: 2px solid black;
+//     border-top-left-radius: 8px;
+//     border-bottom-right-radius: 8px;
+//     width: 25rem;
+//     border-left: none;
+//     border-right: none;
+//     font-size: 18px;
+//     padding: 2rem;
+// `
+
+// export const CondititionSelector = styled.option`
+//     border-top: 1px solid black;
+//     border-bottom: 1px solid black;
+//     width: 30rem;
+//     font-size: 18px;
+// `
