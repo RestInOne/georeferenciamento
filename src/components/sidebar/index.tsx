2997203898b5d8f7c6574649656266c0178ea19e
Vitchenzo2812 <gvitchenzo@gmail.com>
2023-03-03 19:05:35 -0300

    chore: refactor sidebar (need finalize)

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled'
 import { filter } from '../../state/clients'
 import { Filters } from '../../enums/filter'
 import { useRef } from 'react'
 import { useSetRecoilState } from 'recoil'
 
-const SelectItem = React.forwardRef(({ children, ...props }: any, forwardedRef) => {
-  return (
-    <S.Item {...props} ref={forwardedRef}>
-      <S.ItemText>{children}</S.ItemText>
-      <S.ItemIndicator>
-        <S.IconCheck />
-      </S.ItemIndicator>
-    </S.Item>
-  )
-})
 
 function capitalizeWord(str: string) {
   const words = str.split(' ');
@@ -28,83 +18,66 @@ function capitalizeWord(str: string) {
 }
 
 export function Sidebar() {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
 
   const selectorRef = useRef<HTMLSelectElement>(null);
   const setFilters = useSetRecoilState(filter);
 
-  const diseases: string[] = [
+  const conditions: string[] = [
+    'todos5',
     'resistencia_insulina_leve',
     'resistencia_Insulina_Moderada',
-    'resistencia_Insulina_Severo',
+    'resistencia_Insulina_Severo5',
     'diabetico',
     'risco_amputação_leve',
     'risco_moderado',
-    'risco_severo',
-    'doente_metabolico_severo',
-    'doente_metabolico_severo',
-    'doente_metabolico_severo',
+    'risco_severo5',
+    'doente_metabolico_severo5',
     'hipertenso_leve',
     'hipertenso_moderado',
     'hipertenso_severo',
     'risco_pico_hipertensivo',
     'risco_infarto_e_AVC',
-    'risco_evento_cardiaco_grave',
+    'risco_evento_cardiaco_grave5',
     'sobrepeso',
     'obesidade_1',
     'obesidade_2',
-    'obesidade_3',
+    'obesidade_35',
     'stress_elevado',
-    'stress_severo',
-    'sindrome_vagal',
-    'TDAH',
+    'stress_severo5',
+    'sindrome_vagal5',
+    'TDAH5',
     'depressão_leve_ou_moderada',
     'depressão_severa'
   ]
 
   return (
-    <S.Root>
-      <S.Trigger>
-        <S.Value placeholder="Selecione alguma doença..." />
-        <S.SelectIcon>
-          <S.IconDown />
-        </S.SelectIcon>
-      </S.Trigger>
-
-      <S.Portal>
-        <S.Content>
-          <S.ScrollUpButton>
-            <S.IconUp />
-          </S.ScrollUpButton>
-
-          <S.Viewport>
-            {
-              diseases.map((disease, index) => {
-                return (
-                  <S.Group>
-                    <SelectItem value={disease} key={disease[index]}>{capitalizeWord(disease.replace(/_/g, ' '))}</SelectItem>
-                  </S.Group>
-                )
-              })
-            }
-          </S.Viewport>
-          <S.ScrollDownButton>
-            <S.IconDown />
-          </S.ScrollDownButton>
-        </S.Content>
-      </S.Portal>
-    </S.Root>
+    <S.Wrapper>
+      <S.ButtonOpenOrCloseSidebar onClick={() => setIsOpened(old => !old)}>
+        <S.ArrowLeft />
+      </S.ButtonOpenOrCloseSidebar>
+      <S.WrapperConditions openOrCloseSide={isOpened}>
+        <S.LabelTitle>Selecione uma Condição:</S.LabelTitle>
+        <S.Container>
+          {
+            conditions.map((condition, index, conditionArray) => {
+              return (
+                <>
+                  <S.Flex>
+                    <S.CheckBoxRoot id={condition}>
+                      <S.CheckBoxIndicator>
+                        <S.IconCheck />
+                      </S.CheckBoxIndicator>
+                    </S.CheckBoxRoot>
+                    <S.Label>{capitalizeWord(condition.replace(/_/g, ' ').replace('5', ''))}</S.Label>
+                  </S.Flex>
+                  {conditionArray[index].endsWith("5") ? (<S.Separator />) : (<></>)}
+                </>
+              )
+            })
+          }
+        </S.Container>
+      </S.WrapperConditions>
+    </S.Wrapper>
   )
 }
-
-
-    // <S.Nav>
-    //   <S.Label htmlFor='condition'>Selecione o filtro</S.Label>
-    //   <S.ConditionFilter ref={selectorRef} id='condition'>
-    //     <S.CondititionSelector value="diabetes">
-    //       Diabetes
-    //     </S.CondititionSelector>
-    //     <S.CondititionSelector value="adhd">
-    //       Hipertensão
-    //     </S.CondititionSelector>
-    //   </S.ConditionFilter>
-    // </S.Nav>
--- src/components/sidebar/styled.ts
+++ src/components/sidebar/styled.ts
@@ -1,124 +1,107 @@
-import styled from "styled-components";
-import * as Select from '@radix-ui/react-select'
-import { ChevronDownIcon, ChevronUpIcon, CheckIcon } from '@radix-ui/react-icons'
+import styled, { css } from "styled-components";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import * as Selector from "@radix-ui/react-separator"
+import { CheckIcon, ArrowLeftIcon } from "@radix-ui/react-icons";
 
-
-export const IconUp = styled(ChevronUpIcon)`
-    width: 3.5rem;
-    height: 3.5rem;
+export const Wrapper = styled.div`
+    display: flex;
 `
 
-export const IconDown = styled(ChevronDownIcon)`
-    width: 3.5rem;
-    height: 3.5rem;
+export const ButtonOpenOrCloseSidebar = styled.div`
+   
+`
+
+export const ArrowLeft = styled(ArrowLeftIcon)`
+    width: 2rem;
+    height: 2rem;
+    
 `
 
 export const IconCheck = styled(CheckIcon)`
-    width: 3.5rem;
-    height: 3.5rem;
+    width: 2rem;
+    height: 2rem;
 `
 
-export const Root = styled(Select.Root)``
+export const Separator = styled(Selector.Root)`
+    background-color: #383736;
+    margin: .5rem 0;
 
-export const Trigger = styled(Select.SelectTrigger)`
-    all: 'unset';
-    gap: 1rem;
-    border-radius: .4rem;
-    padding: 1rem 3.5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #FFFDF7;
-    color: #73726F;
-    position: absolute;
+    &[data-orientation=horizontal]{
+        height: .1rem;
+        width: 100%;
+    }
+`
+
+export const WrapperConditions = styled.div<{ openOrCloseSide: boolean }>`
+    margin-left: auto;
+    position: fixed;
     top: 2rem;
-    left: 2rem;
-    z-index: 3;
+    right: 2rem;
+    display: none;
+    flex-direction: column;
+    background-color: #DEDBD5;
+    align-items: center;
+    width: max-content;
+    height: max-content;
     font-size: 1.5rem;
-    box-shadow: 0 .2rem 1rem gray;
-    cursor: pointer;
+    z-index: 5;
+    box-shadow: 6px 10px 16px -3px rgba(0,0,0,0.75);
+    border-radius: .8rem;
+    padding: 1.5rem;
+    border: .1rem solid #d4d4d4;
 
-    transition: all .2s ease-in-out;
-    
-    &:hover {
-        background-color: #ECE8EF;
-    }
+    transition: transform .3s ease-in-out
+
+    ${props => props.openOrCloseSide ? css`
+        transform: translateX(1%);
+    ` : css`
+        transform: translateX(100%);
+    `}
 `
 
-export const SelectIcon = styled(Select.SelectIcon)`
-    color: #73726F;
-`
-
-export const Value = styled(Select.Value)``
-
-export const Portal = styled(Select.Portal)`
-    font-size: 1.5rem;
-    background-color: #FFFDF7;
-    z-index: 4;
-`
-
-export const Content = styled(Select.Content)``
-
-export const Viewport = styled(Select.Viewport)``
-
-export const Group = styled(Select.Group)`
+export const Label = styled.label`
+    color: #383736;
+    font-size: 1.7rem;
     padding: 1rem;
-    `
-
-export const Label = styled(Select.Label)``
-
-export const Item = styled(Select.Item)`
-display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    transition: all .2s ease-in-out;
-    
-    &:hover {
-        background-color: #ECE8EF;
-    }
 `
 
-export const ItemText = styled(Select.ItemText)``
+export const LabelTitle = styled(Label)`
+    font-weight: 600;
+`
 
-export const ScrollUpButton = styled(Select.ScrollUpButton)`
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: max-content;
+    gap: .1rem;
+`
+
+export const Flex = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
-    height: max-content;
-    padding: 1rem 0;
-    background-color: #FFFDF7;
-    color: #73726F;
-    cursor: pointer;
-
-    transition: all .2s ease-in-out;
-
-    &:hover {
-        background-color: #ECE8EF;
-    }
 `
 
-export const ScrollDownButton = styled(Select.ScrollDownButton)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: max-content;
-    padding: 1rem 0;
-    background-color: #FFFDF7;
-    color: #73726F;
-    cursor: pointer;
-
-    transition: all .2s ease-in-out;
-
-
-    &:hover {
-        background-color: #ECE8EF;
-    }
-`
-
-export const ItemIndicator = styled(Select.ItemIndicator)`
+export const CheckBoxRoot = styled(Checkbox.Root)`
+    all: 'unset';
+    background-color: #F8F5EE;
     width: 2.5rem;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
+    height: 2.5rem;
+    border-radius: .4rem;
+    display: block;
+    box-shadow: 0 .2rem 1rem #383736;
+    cursor: pointer;
+
+    transition: all .2s ease-in-out;
+
+    &:hover {
+        background-color: #B8B5B0;
+    }
+
+    &:focus {
+        box-shadow: 0 0 0 .2rem #000000;
+    }
+`
+
+export const CheckBoxIndicator = styled(Checkbox.Indicator)`
+    color: #383736;
 `
